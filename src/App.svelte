cc44cdf99ef516b6115a387fb4fcf6e94d06df40
Born-Bohrer <chem1000leaves@gmail.com>
2025-10-07 12:34:47 +0900

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+  const title = 'bf-playground';
+</script>
+
+<svelte:head>
+  <title>{title}</title>
+  <link
+    rel="stylesheet"
+    href="https://api.tktb-tess.dev/fonts/Inter/font-face.css"
+    crossorigin="anonymous"
+  />
+</svelte:head>
+
+<header></header>
