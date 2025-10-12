9ecc2ab5a4ce6369d76755c02e61d7239032d9cf
Born-Bohrer <chem1000leaves@gmail.com>
2025-10-12 18:02:37 +0900

    入力欄追加, デザイン変更

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
   import { exec as exec_ } from '@tktb-tess/brainf_ck-interpreter';
-  import { Result } from 'neverthrow';
+  import { Result, ok } from 'neverthrow';
   const title = 'BF Playground';
   let code = $state(
     '++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.'
   );
+  let input = $state('');
   const exec = Result.fromThrowable(exec_, (e) => {
     if (e instanceof Error) {
       return e;
@@ -14,10 +15,10 @@
   });
 
   const handleClick = () => {
-    result = exec(code);
+    result = exec(code, { input: input });
   };
 
-  let result = $state(exec(''));
+  let result: Result<string, Error> = $state(ok(''));
 </script>
 
 <header>
@@ -28,9 +29,19 @@
   <p>Brainf*ck の簡易的な実行環境です。</p>
 
   <div class="flex flex-col gap-2">
-    <label for="input" class="text-center">コード</label>
-    <textarea id="input" bind:value={code}></textarea>
-    <button type="button" class="self-center" onclick={handleClick}>実行</button>
+    <div class="flex gap-2 *:flex-[1_1_0] *:min-w-0">
+      <div class="flex flex-col">
+        <label for="code" class="text-center">コード</label>
+        <textarea id="code" bind:value={code}></textarea>
+      </div>
+      <div class="flex flex-col">
+        <label for="input" class="text-center">入力</label>
+        <textarea id="input" bind:value={input}></textarea>
+      </div>
+    </div>
+    <button type="button" class="self-center" onclick={handleClick}>
+      実行
+    </button>
     <label for="result" class="text-center">実行結果</label>
     <textarea
       id="result"
