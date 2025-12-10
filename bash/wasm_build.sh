c329a77d765a5df683bd3170855b7843c9a0a3a6
tktb-tess <chemusiclove6174@gmail.com>
2025-12-10 11:18:32 +0900

    test

--- bash/wasm_build.sh
+++ bash/wasm_build.sh
@@ -7,8 +7,7 @@ cd ./rust
 echo "Compiling Rust sources to WebAssembly..."
 cargo build \
     --release \
-    --lib \
-    --target wasm32-unknown-unknown
+    --lib
 
 echo "Executing wasm-bindgen..."
 wasm-bindgen \
