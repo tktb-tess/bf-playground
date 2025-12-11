86fb2580e3a78ff4cbafdff4258b56058abe5a2d
tktb-tess <chemusiclove6174@gmail.com>
2025-12-11 11:31:41 +0900

    修正

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -26,8 +26,12 @@ pub fn exec(code: &str, options: BFExecOptions) -> Result<String, JsError> {
         None => BFMemory::new(),
     };
 
-    let input = options.input.unwrap_or("".into());
-    let mut input: VecDeque<_> = input.into_bytes().into_iter().collect();
+    let mut input: VecDeque<_> = options
+        .input
+        .unwrap_or("".into())
+        .into_bytes()
+        .into_iter()
+        .collect();
     let mut output = vec![];
     let mut i: usize = 0;
     let mut count: u32 = 0;
@@ -59,24 +63,26 @@ pub fn exec(code: &str, options: BFExecOptions) -> Result<String, JsError> {
                 output.push(read);
             }
             Write => {
-                let write = input.pop_front().expect("No input");
+                let write = input
+                    .pop_front()
+                    .ok_or_else(|| BFRuntimeError::new("Ran out of input"))?;
                 *mem.get_value_mut() = write;
             }
             LoopStart => {
                 let mut iter = code.maps.iter();
                 if *mem.get_value() == 0 {
-                    let e = iter
-                        .find(|[s, _]| i == *s)
-                        .ok_or_else(|| BFRuntimeError::new("Unexpected error: loop not found"))?[1];
-                    i = e + 1;
+                    let e = iter.find(|[s, _]| i == *s).ok_or_else(|| {
+                        BFRuntimeError::new("Invalid source code: loop not found")
+                    })?[1];
+                    i = e;
                 }
             }
             LoopEnd => {
                 let mut iter = code.maps.iter();
                 if *mem.get_value() > 0 {
-                    let s = iter
-                        .find(|[_, e]| i == *e)
-                        .ok_or_else(|| BFRuntimeError::new("Unexpected error: loop not found"))?[0];
+                    let s = iter.find(|[_, e]| i == *e).ok_or_else(|| {
+                        BFRuntimeError::new("Invalid source code: loop not found")
+                    })?[0];
                     i = s;
                 }
             }
@@ -84,7 +90,9 @@ pub fn exec(code: &str, options: BFExecOptions) -> Result<String, JsError> {
         i += 1;
         count += 1;
     }
-    let output: String = output.try_into().map_err(|e| BFRuntimeError::new(&format!("{}", e)))?;
+    let output: String = output
+        .try_into()
+        .map_err(|e| BFRuntimeError::new(&format!("{}", e)))?;
     Ok(output)
 }
 
