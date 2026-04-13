b2c7b4dde48803fb85dd91d9043485dd9c1c3ca1
tktb-tess <chemusiclove6174@gmail.com>
2026-04-13 15:46:46 +0900

    separated

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,34 +1,14 @@
+<svelte:options runes />
+
 <script lang="ts">
   import '../app.css';
-  import type { WorkerMsg } from '$lib/util';
-  import { exec } from '$lib/func';
+  import Fields from '$lib/Fields.svelte';
+  import OGP from '$lib/OGP.svelte';
 
   const title = 'BF Playground';
-
-  let code = $state(
-    '++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.',
-  );
-
-  let input = $state('');
-
-  const handleClick = () => {
-    resultA = exec(code, { input });
-  };
-
-  let resultA: Promise<WorkerMsg> = $state(
-    Promise.resolve({ success: true, value: '' }),
-  );
-
-  $effect(() => {
-    resultA
-      .then((e) => {
-        if (!e.success) {
-          console.error(e.error.message);
-        }
-      })
-      .catch((e) => console.error(e));
-  });
 </script>
+
+<OGP {title} url="/bf-playground/" description="brainf*ckのplayground" />
 
 <div class="_cont_">
   <header>
@@ -37,39 +17,7 @@
   <main>
     <p class="back-btn"><a href="https://tktb-tess.github.io">戻る</a></p>
     <p>Brainf*ck の簡易的な実行環境です。</p>
-
-    <div class="bf-playground">
-      <div class="__inputs">
-        <section class="__input-section">
-          <label for="code">コード</label>
-          <textarea id="code" bind:value={code}></textarea>
-        </section>
-        <section class="__input-section">
-          <label for="input">入力</label>
-          <textarea id="input" bind:value={input}></textarea>
-        </section>
-      </div>
-      <button type="button" class="self-center" onclick={handleClick}>
-        実行
-      </button>
-      <div class="__input-section">
-        <label for="result" class="text-center">実行結果</label>
-        {#await resultA}
-          <textarea id="result" readonly>Executing...</textarea>
-        {:then result}
-          <textarea
-            id="result"
-            value={result.success
-              ? result.value
-              : `${result.error.name}: ${result.error.message}`}
-            readonly
-            class={!result.success ? 'text-caution' : ''}
-          ></textarea>
-        {:catch}
-          <textarea id="result" readonly>Unexpected error</textarea>
-        {/await}
-      </div>
-    </div>
+    <Fields />
   </main>
   <footer></footer>
 </div>
@@ -119,26 +67,6 @@
 
     .back-btn {
       @apply mbs-paragraph;
-    }
-
-    .bf-playground {
-      @apply flex flex-col gap-4 mbs-paragraph;
-    }
-
-    .__inputs {
-      @apply grid gap-2 cols-auto-fit-90 *:min-inline-0;
-    }
-
-    .__input-section {
-      @apply flex flex-col gap-1;
-
-      > :where(label) {
-        @apply text-center;
-      }
-
-      > :where(textarea#code) {
-        @apply break-all;
-      }
     }
   }
 </style>
